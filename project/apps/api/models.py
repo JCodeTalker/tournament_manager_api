2fcd3d6346a31391a0c8f5b2c3de7303351f5dea
Jones Soares <jsoares4@lenovo.com>
2024-04-21 23:34:43 -0300

    configuring wip

--- project/apps/api/models.py
+++ project/apps/api/models.py
@@ -1,3 +1,24 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class DecksModel(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE, default=1)
+    
+    name = models.CharField(max_length=65)
+    desc = models.TextField(max_length=600)
+
+    
+    
+    def __str__(self):
+        return self.name
+    
+
+class Cards(models.Model):
+    deck = models.ForeignKey(DecksModel, on_delete=models.CASCADE)
+    name = models.CharField(max_length=65)
+    desc = models.CharField(max_length=200)
+    api_id = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return super().__str__()
